48dfea65bf4189134e26de27bfeeba070cdb46e4
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2020-12-11 13:08:35 +0300

    Port better constantize method from Sidekiq

--- lib/sidekiq/grouping/middleware.rb
+++ lib/sidekiq/grouping/middleware.rb
@@ -4,7 +4,7 @@ module Sidekiq
       def call(worker_class, msg, queue, redis_pool = nil)
         return yield if (defined?(Sidekiq::Testing) && Sidekiq::Testing.inline?)
 
-        worker_class = worker_class.camelize.constantize if worker_class.is_a?(String)
+        worker_class = constantize(worker_class.camelize) if worker_class.is_a?(String)
         options = worker_class.get_sidekiq_options
 
         batch =
@@ -37,6 +37,20 @@ module Sidekiq
           .add(msg['args'])
 
         nil
+      end
+
+      # https://github.com/mperham/sidekiq/blob/v6.0.7/lib/sidekiq/processor.rb#L267
+      def constantize(str)
+        return Object.const_get(str) unless str.include?("::")
+
+        names = str.split("::")
+        names.shift if names.empty? || names.first.empty?
+
+        names.inject(Object) do |constant, name|
+          # the false flag limits search for name to under the constant namespace
+          #   which mimics Rails' behaviour
+          constant.const_get(name, false)
+        end
       end
     end
   end
